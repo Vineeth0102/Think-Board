8853664b5b37f1b915078c0c5593d379c433c5a9
vineeth shettigar <vineethshettigar0102@gmail.com>
2025-06-13 15:43:35 +0530

    Initial commit

---
+++ backend/src/routes/notesRoutes.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { createNote, deleteNote, getAllNotes, updateNote, getNoteByID } from "../Controller/notesController.js";
+
+const router = express.Router();
+
+router.get("/",getAllNotes);
+router.get("/:id",getNoteByID);
+router.post("/",createNote);
+router.put("/:id",updateNote);
+router.delete("/:id",deleteNote);
+
+
+export default router;
